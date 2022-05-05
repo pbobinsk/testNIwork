2f7bc5e0cc7acb6329dd7b18588347b08fa518db
Piotr Bobiński <piotr.bobinski@wsb.warszawa.pl>
2022-05-05 18:02:57 +0200

    pierwszy commit użytkownika pbobinsk

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello WSB");
+
+        Klasa1 klasa1 = new Klasa1();
+
+        System.out.println(klasa1);
     }
 }
