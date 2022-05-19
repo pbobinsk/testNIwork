4580fc94663bbea8315aadcb5ee739bde42cbdf4
Piotr Bobiński <piotr.bobinski@wsb.warszawa.pl>
2022-05-19 18:29:00 +0200

    konflikt 1 pbobinsk

---
+++ src/conflict/Konflikt.java
@@ -0,0 +1,17 @@
+package conflict;
+
+public class Konflikt {
+
+    private int int1 = 123;
+
+    public Konflikt(int int1) {
+        this.int1 = int1;
+    }
+
+    @Override
+    public String toString() {
+        return "Konflikt{" +
+                "int1=" + int1 +
+                '}';
+    }
+}
