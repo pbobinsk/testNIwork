74d26f50479a48bf7b47b8b2a44fae85dffe5746
Piotr Bobiński <piotr.bobinski@wsb.warszawa.pl>
2022-05-19 17:54:10 +0200

    merge 2gi WSB

--- src/KlasaMerge.java
+++ src/KlasaMerge.java
@@ -2,14 +2,22 @@ public class KlasaMerge {
 
     private String s1;
 
+    private String s2;
+
     public KlasaMerge(String s1) {
         this.s1 = s1;
+    }
+
+    public KlasaMerge(String s1, String s2) {
+        this.s1 = s1;
+        this.s2 = s2;
     }
 
     @Override
     public String toString() {
         return "KlasaMerge{" +
                 "s1='" + s1 + '\'' +
+                ", s2='" + s2 + '\'' +
                 '}';
     }
 }
