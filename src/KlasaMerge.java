a4877eec866a820ed05f30f85fe0b2e2e2302bae
Piotr Bobiński <piotr.bobinski@wsb.warszawa.pl>
2022-05-19 17:56:03 +0200

    merge 1szy pbobinsk

--- src/KlasaMerge.java
+++ src/KlasaMerge.java
@@ -1,15 +1,23 @@
 public class KlasaMerge {
 
-    private String s1;
+    private double s1;
 
-    public KlasaMerge(String s1) {
+    private double d2;
+
+    public KlasaMerge(double s1) {
         this.s1 = s1;
+    }
+
+    public KlasaMerge(double s1, double d2) {
+        this.s1 = s1;
+        this.d2 = d2;
     }
 
     @Override
     public String toString() {
         return "KlasaMerge{" +
-                "s1='" + s1 + '\'' +
+                "s1=" + s1 +
+                ", d2=" + d2 +
                 '}';
     }
 }
